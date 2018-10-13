fce808a8c7d604c58c7719f901ea30fdb9e59b34
cjabswalsh <cjwalsh314@gmail.com>
2018-10-13 00:40:24 -0400

    added basic deck initialization and printing

--- deck.py
+++ deck.py
@@ -1,5 +1,30 @@
 from card import Card
+from colors import Color
+import random
 
 class Deck:
   cards = []
-  
+  numbers = [1, 1, 1, 2, 2, 3, 3, 4, 4, 5]
+
+  def __init__(self):
+    self.cards = []
+    self.fill_deck()
+    self.shuffle()
+
+  def fill_deck(self):
+    for color in Color:
+      for number in self.numbers:
+        self.cards.append(Card(color, number))
+
+  def shuffle(self):
+    random.shuffle(self.cards)
+
+  def draw(self, number):
+    ret = []
+    for i in range(number):
+      ret.append(self.cards.pop())
+    return ret
+
+  def print_deck(self):
+    for card in self.cards:
+      print(card)
